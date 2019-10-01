5cfeac095ebcef6932057e95600a533dd2b2d046
Eduardo Santana <eduardo_amparo@hotmail.com>
2019-09-30 22:25:23 -0400

    Fixing modules include file.

--- dicom_util/src/main/java/com/google/cloud/healthcare/Annotations.java
+++ dicom_util/src/main/java/com/google/cloud/healthcare/Annotations.java
@@ -20,11 +20,19 @@ import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import com.google.inject.BindingAnnotation;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-/** Contains annotations used in util/ **/
-public class Annotations {
-  @BindingAnnotation @Target({ FIELD, PARAMETER, METHOD }) @Retention(RUNTIME)
-  public @interface DicomwebAddr {}
+/**
+ * Contains annotations used in util/
+ **/
+public class Annotations
+{
+    @BindingAnnotation
+    @Target({FIELD, PARAMETER, METHOD})
+    @Retention(RUNTIME)
+    public @interface DicomwebAddr
+    {
+    }
 }
