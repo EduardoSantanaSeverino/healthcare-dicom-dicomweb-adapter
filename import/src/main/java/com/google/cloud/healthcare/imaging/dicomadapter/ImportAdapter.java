015d51368256fc70fa4a368ba23d17b34af9616e
Eduardo Santana <eduardo_amparo@hotmail.com>
2019-09-30 23:09:39 -0400

    Adding fake data!

--- import/src/main/java/com/google/cloud/healthcare/imaging/dicomadapter/ImportAdapter.java
+++ import/src/main/java/com/google/cloud/healthcare/imaging/dicomadapter/ImportAdapter.java
@@ -26,9 +26,11 @@ import com.google.cloud.healthcare.LogUtil;
 import com.google.cloud.healthcare.imaging.dicomadapter.cstoresender.CStoreSenderFactory;
 import com.google.cloud.healthcare.imaging.dicomadapter.monitoring.Event;
 import com.google.cloud.healthcare.imaging.dicomadapter.monitoring.MonitoringService;
+
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
+
 import org.dcm4che3.net.Device;
 import org.dcm4che3.net.service.BasicCEchoSCP;
 import org.dcm4che3.net.service.DicomServiceRegistry;
@@ -36,83 +38,89 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class ImportAdapter {
+public class ImportAdapter
+{
 
-  private static Logger log = LoggerFactory.getLogger(ImportAdapter.class);
+    private static Logger log = LoggerFactory.getLogger(ImportAdapter.class);
 
-  public static void main(String[] args) throws IOException, GeneralSecurityException {
-    boolean isDebug = false;
-    Flags flags = new Flags();
-    if (isDebug)
+    public static void main(String[] args) throws IOException, GeneralSecurityException
     {
-      flags = new Flags();
+        boolean isDebug = false;
+        Flags flags = new Flags();
+        if (isDebug)
+        {
+            flags = new Flags("TestDicom1","Test",5678,"","","","","","","");
+        } else
+        {
+            JCommander jCommander = new JCommander(flags);
+            jCommander.parse(args);
+        }
+
+        // Adjust logging.
+        if (flags.verbose)
+        {
+            LogUtil.Log4jToStdout();
+        }
+
+        // Credentials, use the default service credentials.
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+        if (!flags.oauthScopes.isEmpty())
+        {
+            credentials = credentials.createScoped(Arrays.asList(flags.oauthScopes.split(",")));
+        }
+
+        HttpRequestFactory requestFactory =
+                new NetHttpTransport().createRequestFactory(new HttpCredentialsAdapter(credentials));
+
+        // Initialize Monitoring
+        if (!flags.monitoringProjectId.isEmpty())
+        {
+            MonitoringService.initialize(flags.monitoringProjectId, Event.values(), requestFactory);
+            MonitoringService.addEvent(Event.STARTED);
+        } else
+        {
+            MonitoringService.disable();
+        }
+
+        // Dicom service handlers.
+        DicomServiceRegistry serviceRegistry = new DicomServiceRegistry();
+
+        // Handle C-ECHO (all nodes which accept associations must support this).
+        serviceRegistry.addDicomService(new BasicCEchoSCP());
+
+        // Handle C-STORE
+        String cstoreDicomwebAddr = flags.dicomwebAddress;
+        String cstoreDicomwebStowPath = "studies";
+        if (cstoreDicomwebAddr.length() == 0)
+        {
+            cstoreDicomwebAddr = flags.dicomwebAddr;
+            cstoreDicomwebStowPath = flags.dicomwebStowPath;
+        }
+
+        IDicomWebClient cstoreDicomWebClient =
+                new DicomWebClientJetty(credentials, cstoreDicomwebAddr);
+        CStoreService cStoreService =
+                new CStoreService(cstoreDicomwebStowPath, cstoreDicomWebClient);
+        serviceRegistry.addDicomService(cStoreService);
+
+        // Handle C-FIND
+        IDicomWebClient dicomWebClient =
+                new DicomWebClient(requestFactory, flags.dicomwebAddress);
+        CFindService cFindService = new CFindService(dicomWebClient);
+        serviceRegistry.addDicomService(cFindService);
+
+        // Handle C-MOVE
+        String cstoreSubAet = flags.dimseCmoveAET.equals("") ? flags.dimseAET : flags.dimseCmoveAET;
+        CStoreSenderFactory cStoreSenderFactory = new CStoreSenderFactory(cstoreSubAet, dicomWebClient);
+        AetDictionary aetDict = new AetDictionary(flags.aetDictionaryInline, flags.aetDictionaryPath);
+        CMoveService cMoveService = new CMoveService(dicomWebClient, aetDict, cStoreSenderFactory);
+        serviceRegistry.addDicomService(cMoveService);
+
+        // Handle Storage Commitment N-ACTION
+        serviceRegistry.addDicomService(new StorageCommitmentService(dicomWebClient, aetDict));
+
+        // Start DICOM server
+        Device device = DeviceUtil.createServerDevice(flags.dimseAET, flags.dimsePort, serviceRegistry);
+        device.bindConnections();
     }
-    else
-    {
-      JCommander jCommander = new JCommander(flags);
-      jCommander.parse(args);
-    }
-
-    // Adjust logging.
-    if (flags.verbose) {
-      LogUtil.Log4jToStdout();
-    }
-
-    // Credentials, use the default service credentials.
-    GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
-    if (!flags.oauthScopes.isEmpty()) {
-      credentials = credentials.createScoped(Arrays.asList(flags.oauthScopes.split(",")));
-    }
-
-    HttpRequestFactory requestFactory =
-        new NetHttpTransport().createRequestFactory(new HttpCredentialsAdapter(credentials));
-
-    // Initialize Monitoring
-    if (!flags.monitoringProjectId.isEmpty()) {
-      MonitoringService.initialize(flags.monitoringProjectId, Event.values(), requestFactory);
-      MonitoringService.addEvent(Event.STARTED);
-    } else {
-      MonitoringService.disable();
-    }
-
-    // Dicom service handlers.
-    DicomServiceRegistry serviceRegistry = new DicomServiceRegistry();
-
-    // Handle C-ECHO (all nodes which accept associations must support this).
-    serviceRegistry.addDicomService(new BasicCEchoSCP());
-
-    // Handle C-STORE
-    String cstoreDicomwebAddr = flags.dicomwebAddress;
-    String cstoreDicomwebStowPath = "studies";
-    if (cstoreDicomwebAddr.length() == 0) {
-      cstoreDicomwebAddr = flags.dicomwebAddr;
-      cstoreDicomwebStowPath = flags.dicomwebStowPath;
-    }
-
-    IDicomWebClient cstoreDicomWebClient =
-        new DicomWebClientJetty(credentials, cstoreDicomwebAddr);
-    CStoreService cStoreService =
-        new CStoreService(cstoreDicomwebStowPath, cstoreDicomWebClient);
-    serviceRegistry.addDicomService(cStoreService);
-
-    // Handle C-FIND
-    IDicomWebClient dicomWebClient =
-        new DicomWebClient(requestFactory, flags.dicomwebAddress);
-    CFindService cFindService = new CFindService(dicomWebClient);
-    serviceRegistry.addDicomService(cFindService);
-
-    // Handle C-MOVE
-    String cstoreSubAet = flags.dimseCmoveAET.equals("") ? flags.dimseAET : flags.dimseCmoveAET;
-    CStoreSenderFactory cStoreSenderFactory = new CStoreSenderFactory(cstoreSubAet, dicomWebClient);
-    AetDictionary aetDict = new AetDictionary(flags.aetDictionaryInline, flags.aetDictionaryPath);
-    CMoveService cMoveService = new CMoveService(dicomWebClient, aetDict, cStoreSenderFactory);
-    serviceRegistry.addDicomService(cMoveService);
-
-    // Handle Storage Commitment N-ACTION
-    serviceRegistry.addDicomService(new StorageCommitmentService(dicomWebClient, aetDict));
-
-    // Start DICOM server
-    Device device = DeviceUtil.createServerDevice(flags.dimseAET, flags.dimsePort, serviceRegistry);
-    device.bindConnections();
-  }
 }
